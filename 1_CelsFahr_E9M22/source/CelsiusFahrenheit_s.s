a95fb7b867b5460b5748190d3c359342e2d7fc84
Albertmarti-urv <albertmarg@gmail.com>
2025-04-20 20:26:03 +0200

    Update CelsiusFahrenheit_s.s

--- 1_CelsFahr_E9M22/source/CelsiusFahrenheit_s.s
+++ 1_CelsFahr_E9M22/source/CelsiusFahrenheit_s.s
@@ -1,35 +1,61 @@
     .section .data
     .align 4
 
-@ Constantes codificades en E9M22 (convertides des de float en C)
+@ Constants codificades en E9M22 (convertides des de float en C)
 c_9_over_5:
-    .word 0x40e66666   @ float(1.8) ? E9M22 (aproximadament)
+    .word 0x40e66666   @ float(1.8) ˜ E9M22
 
 c_32:
-    .word 0x42000000   @ float(32.0) ? E9M22
+    .word 0x42000000   @ float(32.0) ˜ E9M22
+
+c_5_over_9:
+    .word 0x3f124925   @ float(5/9) ˜ 0.555... en E9M22
+
 
     .section .text
     .global Celsius2Fahrenheit
     .type Celsius2Fahrenheit, %function
 
 Celsius2Fahrenheit:
-    push {lr}                      @ Guardem l�adre�a de retorn
+    push {lr}                      @ Guardem l’adreça de retorn
 
-    ldr r1, =c_9_over_5            @ Carreguem l�adre�a de la constant 9/5
+    ldr r1, =c_9_over_5            @ Carreguem l’adreça de la constant 9/5
     ldr r1, [r1]                   @ r1 = E9M22(9/5)
-
-    mov r0, r0                     @ r0 = input (ja ve com a par�metre)
 
     bl E9M22_mul                   @ r0 = input * 9/5
 
     mov r2, r0                     @ guardem el resultat parcial a r2
 
-    ldr r1, =c_32                  @ carreguem l�adre�a de la constant 32
+    ldr r1, =c_32                  @ carreguem l’adreça de la constant 32
     ldr r1, [r1]                   @ r1 = E9M22(32)
 
     mov r0, r2                     @ r0 = resultat anterior
 
     bl E9M22_add                   @ r0 = (input * 9/5) + 32
 
-    pop {lr}                       @ Recuperem l�adre�a de retorn
+    pop {lr}                       @ Recuperem l’adreça de retorn
+    bx lr                          @ Retornem (resultat en r0)
+
+
+    .global Fahrenheit2Celsius
+    .type Fahrenheit2Celsius, %function
+
+Fahrenheit2Celsius:
+    push {lr}                      @ Guardem l’adreça de retorn
+
+    ldr r1, =c_32                  @ Carreguem l’adreça de la constant 32
+    ldr r1, [r1]                   @ r1 = E9M22(32)
+
+    bl E9M22_sub                   @ r0 = input - 32
+
+    mov r2, r0                     @ guardem el resultat parcial a r2
+
+    ldr r1, =c_5_over_9            @ carreguem l’adreça de la constant 5/9
+    ldr r1, [r1]                   @ r1 = E9M22(5/9)
+
+    mov r0, r2                     @ r0 = resultat anterior
+
+    bl E9M22_mul                   @ r0 = (input - 32) * 5/9
+
+    pop {lr}                       @ Recuperem l’adreça de retorn
     bx lr                          @ Retornem (resultat en r0)
